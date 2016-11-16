9f4345b2caaac4c25cbf1a0b425385ff86d82acb
Grant Klinsing <grant@digitalopera.com>
2016-11-16 07:56:10 -0600

    Add enter/leave hooks to simple router

--- src/Router.js
+++ src/Router.js
@@ -8,21 +8,62 @@ class Router extends Component {
 
     this.state = {
       component: null,
-      context: null
+      context: null,
+      onEnter: null,
+      onLeave: null,
+      path: null
     }
   }
 
   componentDidMount() {
-    this.props.routes.forEach(route => {
-      createRoute(route.path, (context) => {
+    this.props.routes.forEach((route) => {
+      const {
+        component,
+        onEnter,
+        onLeave,
+        path
+      } = route
+      createRoute(path, (context) => {
         this.setState({
-          component: route.component,
-          context
+          component,
+          context,
+          onEnter,
+          onLeave,
+          path
         })
       })
     })
 
     beginRouting()
+  }
+
+  /*
+   * since this component will update only when the path
+   * changes, componentWillUpdate will only be called on
+   * route change 
+   */
+  componentWillUpdate(){
+    if(this.state.onLeave){
+      this.state.onLeave()
+    }
+  }
+  
+  /*
+   * since this component will update only when the path
+   * changes, componentDidUpdate will only be called on render
+   */
+  componentDidUpdate(){
+    if(this.state.onEnter) {
+      this.state.onEnter()
+    }
+  }
+
+  /*
+   * if the current state path matches the nextState path
+   * do not update the component
+   */
+  shouldComponentUpdate(nextProps, nextState){
+    return this.state.path !== nextState.path
   }
 
   render() {
--- src/__tests__/Router-test.js
+++ src/__tests__/Router-test.js
@@ -69,3 +69,97 @@ test('route information should exist on props', () => {
   const tree = component.toJSON()
   expect(tree).toMatchSnapshot()
 })
+
+test('onEnter is called when component is rendered', () => {
+  const createRouteCallbacks = {}
+  const onEnterCallback = jest.fn()
+
+  createRoute.mockImplementation((path, callback) => {
+    createRouteCallbacks[path] = callback
+  })
+
+  const Home = () => <div>Home</div>
+
+  const routes = [
+    { path: '/', component: Home, onEnter: onEnterCallback },
+  ]
+
+  renderer.create(<Router routes={routes} />)
+
+  createRouteCallbacks['/']()
+  expect(onEnterCallback).toHaveBeenCalled()
+})
+
+test('onLeave is called when component is unmounted', () => {
+  const createRouteCallbacks = {}
+  const onLeaveCallback = jest.fn()
+
+  createRoute.mockImplementation((path, callback) => {
+    createRouteCallbacks[path] = callback
+  })
+
+  const Home = () => <div>Home</div>
+
+  const routes = [
+    { path: '/', component: Home, onLeave: onLeaveCallback },
+    { path: '/home2', component: Home },
+  ]
+
+  renderer.create(<Router routes={routes} />)
+
+  createRouteCallbacks['/']()
+  expect(onLeaveCallback).not.toHaveBeenCalled()
+  createRouteCallbacks['/home2']()
+  expect(onLeaveCallback).toHaveBeenCalled()
+})
+
+test('previous component onLeave is called next component onEnter', () => {
+  const createRouteCallbacks = {}
+  const onLeaveCallback = jest.fn()
+  const onEnterCallback = jest.fn()
+  const called = []
+
+  createRoute.mockImplementation((path, callback) => {
+    createRouteCallbacks[path] = callback
+  })
+  onLeaveCallback.mockImplementation(() => {
+    called.push('onLeave')
+  })
+  onEnterCallback.mockImplementation(() => {
+    called.push('onEnter')
+  })
+  const Home = () => <div>Home</div>
+
+  const routes = [
+    { path: '/', component: Home, onLeave: onLeaveCallback },
+    { path: '/home2', component: Home, onEnter: onEnterCallback },
+  ]
+
+  renderer.create(<Router routes={routes} />)
+
+  createRouteCallbacks['/']()
+  createRouteCallbacks['/home2']()
+  expect(called).toEqual(['onLeave','onEnter'])
+})
+
+test('it doesn\'t call onEnter/onLeave if same state is called', () => {
+  const createRouteCallbacks = {}
+  const onLeaveCallback = jest.fn()
+  const onEnterCallback = jest.fn()
+
+  createRoute.mockImplementation((path, callback) => {
+    createRouteCallbacks[path] = callback
+  })
+  const Home = () => <div>Home</div>
+
+  const routes = [
+    { path: '/', component: Home, onEnter: onEnterCallback, onLeave: onLeaveCallback },
+  ]
+
+  renderer.create(<Router routes={routes} />)
+
+  createRouteCallbacks['/']()
+  createRouteCallbacks['/']()
+  expect(onEnterCallback).toHaveBeenCalledTimes(1)
+  expect(onLeaveCallback).toHaveBeenCalledTimes(0)
+})
